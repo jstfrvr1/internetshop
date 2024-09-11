e1e9f50062728e21bddddad3aeb5bad3c88b0bbc
jstfrvr1 <loltestmailru@mail.ru>
2024-09-12 01:21:41 +0500

    v2

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home(Request):
-    return HttpResponse("Hi!")
+def home(request):
+    return render(request, "index.html")
