9218a3c74e9401297cb7df1ecd6d28649e96f6e9
jstfrvr1 <loltestmailru@mail.ru>
2024-09-12 02:38:50 +0500

    v5

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -9,3 +9,10 @@ def home(request):
     return render(request, "index.html", {
         'products': products
     })
+
+def view_product(request, id):
+    product = Product.objects.filter(id=id).first()
+
+    return render(request, 'product.html', {
+        'product': product
+    })
