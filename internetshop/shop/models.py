c3153677d1d7cc8972d8d66068196ee627e49714
jstfrvr1 <loltestmailru@mail.ru>
2024-09-12 02:20:47 +0500

    v4

--- internetshop/shop/models.py
+++ internetshop/shop/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=128)
-    description = models.CharField(max_length=128)
+    description = models.TextField()
     price = models.IntegerField()
     image_url = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'{self.name}'
